4ee44ec5ed4e0cd841dc7a822a9d12a4c0d88772
liuzhishan <liuzhishan01@gmail.com>
2023-10-16 12:29:50 +0800

    fix logic for find target-filename

--- evil-gf.el
+++ evil-gf.el
@@ -2,7 +2,6 @@
 ;;;
 ;;; open-file-under-cursor for evil mode.
 ;;;
-;;;-*- lexical-binding: t; -*-
 ;;
 ;; Copyright (C) 2023 liuzhishan
 ;;
@@ -25,19 +24,21 @@
 ;;; Code:
 (defun evil-gf-open-file-under-cursor ()
   (interactive)
-  (let ((file-path (thing-at-point 'filename t)))
+  (let* ((file-path (thing-at-point 'filename t)))
     (if (file-name-absolute-p file-path)
-        (find-file file-path)
-      (let ((arr (split-string (buffer-file-name) "/"))
-            (len-arr (number-sequence 1 (length arr)))
-            (target-filename ""))
-        (dolist (len (number-sequence 1 (length arr)) target-filename)
-          (let ((new-filename (string-join (append (-take len arr) (list file-path)) "/")))
-            (if (file-exists-p new-filename)
-                (setq target-filename new-filename))
-            ))
+    (find-file file-path)
+      (let* ((parts (split-string (buffer-file-name) "/"))
+        (target-filename "")
+        (target-exists nil))
 
-        (if (and (> (length target-filename) 5) (file-exists-p target-filename))
+        (dolist (n (number-sequence 1 (length parts)) target-filename)
+          (let* ((new-filename (string-join (append (-take n parts) (list file-path)) "/")))
+            (if (file-exists-p new-filename)
+                (setq target-filename new-filename
+                    target-exists t)
+            )))
+
+        (if target-exists
             (find-file target-filename)
           (message (format "file not exists: %s" file-path)))))))
 
